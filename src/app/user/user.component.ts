a9890aede7cb209f224aa7b6b8ac63fe54d545c8
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-11-14 17:08:38 +0100

    added css media query for main section

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,18 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'users/' + this.selectedUser().avatar)
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // get imagePath() {
-  //   return 'users/' + this.selectedUser.avatar;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set( DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return '/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
